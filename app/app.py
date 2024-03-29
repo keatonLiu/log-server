7622d67d04fb422f6f426ac53408019e6e7d351e
keatonLiu <lxtxiaotong@foxmail.com>
2024-03-29 19:37:44 +0800

    exception

--- app/app.py
+++ app/app.py
@@ -7,9 +7,14 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/log')
 def show_log():
     fileuri = request.args.get('fileuri')
-    res = requests.get(fileuri)
+    if not fileuri:
+        return 'fileuri is required', 400
+    try:
+        res = requests.get(fileuri)
+    except requests.exceptions.RequestException as e:
+        return str(e), 500
     return Response(res.content.decode('utf-8'), mimetype='text/plain', content_type='text/plain; charset=utf-8')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
