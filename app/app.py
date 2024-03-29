905c672681bccf5a597ea8bc5f5b9ac7be0f7307
keatonLiu <lxtxiaotong@foxmail.com>
2024-03-29 20:04:43 +0800

    use js

--- app/app.py
+++ app/app.py
@@ -1,19 +1,11 @@
-import requests
-from flask import Flask, request, Response
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/log')
 def show_log():
-    fileuri = request.args.get('fileuri')
-    if not fileuri:
-        return 'fileuri is required', 400
-    try:
-        res = requests.get(fileuri)
-    except requests.exceptions.RequestException as e:
-        return str(e), 500
-    return Response(res.content.decode('utf-8'), mimetype='text/plain', content_type='text/plain; charset=utf-8')
+    return render_template('log.html')
 
 
 if __name__ == '__main__':
